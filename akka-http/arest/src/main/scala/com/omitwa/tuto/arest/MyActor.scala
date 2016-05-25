66588c7d0c437a0018a9b9acf509b5471d318f36
Sunil Mandowara <skmandowara@gmail.com>
2016-05-25 23:50:38 +0530

    akka rest

---
+++ akka-http/arest/src/main/scala/com/omitwa/tuto/arest/MyActor.scala
@@ -0,0 +1,26 @@
+package com.omitwa.tuto.arest
+
+import akka.actor.Actor
+
+/**
+ * @author sunil
+
+ */
+
+case class USER_ID()
+case class ID(id:Int)
+case class USER_JOBS()
+case class UserJob(userName:String,numberOfReq:Int)
+    
+class MyActor extends Actor{
+  var id:Int =0;
+  def receive = {
+    case USER_ID => id=id+1
+                  sender ! ID(id)
+    case USER_JOBS => 
+                   val x:List[UserJob]=List(UserJob("tunu",1),UserJob("munu",2))
+                  //sender ! ID(count)
+                  sender ! x
+    case _ => println("unknown request")
+  }
+}
